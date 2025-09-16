fe7a7fefe65da4ad4113d1bfde3a48eefc50cb1d
Anand Sogalad <anand.sogalad305@gmail.com>
2025-09-16 06:15:28 +0530

    Implement Mobile class with methods for initialization, turning on/off, and calling; add examples in main function.

--- module1/oop1.py
+++ module1/oop1.py
@@ -33,6 +33,50 @@ class Duck(object):
         return self.__sex
 
 
+"""
+Scenario
+create a class representing a mobile phone;
+your class should implement the following methods:
+__init__ expects a number to be passed as an argument; this method stores the number in an instance variable self.number
+turn_on() should return the message 'mobile phone {number} is turned on'. Curly brackets are used to mark the place to insert the object's number variable;
+turn_off() should return the message 'mobile phone is turned off';
+call(number) should return the message 'calling {number}'. Curly brackets are used to mark the place to insert the object's number variable;
+create two objects representing two different mobile phones; assign any random phone numbers to them;
+implement a sequence of method calls on the objects to turn them on, call any number. Print the methods' outcomes;
+turn off both mobiles.
+Example output
+
+mobile phone 01632-960004 is turned on
+mobile phone 01632-960012 is turned on
+calling 555-34343
+mobile phone is turned off
+mobile phone is turned off
+
+"""
+
+
+class Mobile(object):
+    def __init__(self, number):
+        self.number = number
+
+    @property
+    def number(self):
+        return self.__number
+
+    @number.setter
+    def number(self, number):
+        self.__number = number
+
+    def turn_on(self):
+        return f"mobile phone {self.number} is turned on"
+
+    def turn_off(self):
+        return "mobile phone is turned off"
+
+    def call(self, number):
+        return f"calling {number}"
+
+
 def main():
     # creating instances of the abovve class
     duckling = Duck(10, 3.4, "male")
@@ -53,6 +97,24 @@ def main():
     print(duckling.walk(), duckling.quack())
     print(drake.walk(), drake.quack())
     print(hen.walk(), hen.quack())
+
+    # types
+    print(Duck.__class__)
+    print(duckling.__class__)
+    print(duckling.sex.__class__)
+    print(duckling.quack.__class__)
+
+    # mobile phone examples
+    x_phone = Mobile("+91 99858 32548")
+    y_phone = Mobile("+91 99858 32549")
+
+    print(x_phone.turn_on())
+    print(y_phone.turn_on())
+
+    print(x_phone.call(y_phone.number))
+
+    print(x_phone.turn_off())
+    print(y_phone.turn_off())
 
 
 if __name__ == "__main__":
