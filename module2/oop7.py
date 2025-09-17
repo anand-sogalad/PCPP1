0513cafd4518978a7229c263155f5beb52f55d22
Anand Sogalad <anand.sogalad305@gmail.com>
2025-09-17 07:55:32 +0530

    decorator as class

--- module2/oop7.py
+++ module2/oop7.py
@@ -16,7 +16,32 @@ class Decorator(object):
         return result
 
 
-@Decorator
+class Decorator1:
+    def __init__(self, func=None, *, decorator_arg="SomeValue"):
+        self.func = func
+        self.arg = decorator_arg
+
+    def __call__(self, *args, **kwargs):
+        # If func is not None, this is decorating a function
+        if self.func:
+            print(
+                f"I am decorator class and this is printing from {self.__class__.__name__} with {self.arg}"
+            )
+            result = self.func(*args, **kwargs)
+            print(result)
+            print(
+                f"I am decorator class and this is printing from {self.__class__.__name__} with {self.arg}"
+            )
+            return result
+        else:
+            # If no function yet, return a decorator factory
+            def real_decorator(func):
+                return Decorator1(func, decorator_arg=self.arg)
+
+            return real_decorator
+
+
+@Decorator1
 def myfunc(a, b):
     return a + b
 
